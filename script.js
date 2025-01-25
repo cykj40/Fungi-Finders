e42900943757e2e22b0c7592586ac99004fa37a2
Cyrus Jalili Khiabani <102045473+cykj40@users.noreply.github.com>
2025-01-25 07:38:05 -0500

    working on the other pages of the app

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
+console.log('Script loaded');
+
 const navToggle = document.querySelector('.mobile-nav-toggle');
 const primaryNav = document.querySelector('.primary-navigation');
 
+console.log('navToggle:', navToggle);
+console.log('primaryNav:', primaryNav);
+
 navToggle.addEventListener('click', () => {
+    console.log('Button clicked!');
     const visibility = primaryNav.getAttribute('data-visible');
 
     if (visibility === "false") {
